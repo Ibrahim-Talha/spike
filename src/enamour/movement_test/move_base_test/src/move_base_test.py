72a4c8ac1d711f8c67503bb0254f9ec80326f58b
lars <you@example.com>
2022-04-26 21:51:50 +0200

    Rotation add

--- src/enamour/movement_test/move_base_test/src/move_base_test.py
+++ src/enamour/movement_test/move_base_test/src/move_base_test.py
@@ -3,7 +3,13 @@
 
 import rospy
 import actionlib
+import numpy as np
+import math
+from tf.transformations import quaternion_from_euler
+from tf.transformations import *
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+
 
 def movebase_client():
 
@@ -17,9 +23,20 @@ def movebase_client():
     goal.target_pose.header.stamp = rospy.Time.now()
     x_input = float(input("Input meters to move forwards/backwards: "))
     y_input = float(input("Input meters to move left/right: "))
+    degree = float(input("how much degree: "))
+
+    q_rot = quaternion_from_euler( 0, 0, np.radians(degree) ) # wandelt den float in radianten und bildet daraus von euler winkel in quationen um
+
     goal.target_pose.pose.position.x = x_input
     goal.target_pose.pose.position.y = y_input
-    goal.target_pose.pose.orientation.w = 1.0
+    
+    goal.target_pose.pose.orientation.x = q_rot[0] # setzt qx-quaternion Wert ein
+    goal.target_pose.pose.orientation.y = q_rot[1] # setzt qy-quaternion Wert ein
+    goal.target_pose.pose.orientation.z = q_rot[2] # setzt qz-quaternion Wert ein
+    goal.target_pose.pose.orientation.w = q_rot[3] # setzt qw-quaternion Wert ein
+
+
+  
 
     print("Sending command to action server")
     client.send_goal(goal)
@@ -30,6 +47,10 @@ def movebase_client():
         rospy.signal_shutdown("Action server not available!")
     else:
         return client.get_result()
+
+
+
+
 
 if __name__ == '__main__':
     try:
